e8adc52e9a659f1bdae08c6706cf85d3ac2fea52
Vyom Fadia <vyom.fadia@outlook.com>
2018-09-03 11:55:12 +0100

    Rewrite devutils, integrating some of @JosiahWI 's work.

--- gen.py
+++ gen.py
@@ -5,6 +5,6 @@ print ( "------------------ GENERATING CMAKE ------------------" )
 script_dir = str( os.path.dirname( os.path.realpath( __file__ ) ) )
 
 root_dir = script_dir + "/../"
-cmake_dir = script_dir + "/cmake/"
+cmake_dir = script_dir + "/resources/cmake/"
 
 shutil.copyfile ( cmake_dir + "root.cmake", root_dir + "CMakeLists.txt" )
